ed5a0536be429942829425d581d52e9ea25908cf
RobertDQuant <Robertdquant@gmail.com>
2017-02-24 14:47:45 -0500

    Update sketch.js

--- Rob_18_functionsDefined/sketch.js
+++ Rob_18_functionsDefined/sketch.js
@@ -2,20 +2,20 @@
 https://vimeo.com/channels/learningp5js/139587733
 */
 var ball = { //this is where the eye starts from
-  x: 300, 
-  y: 200, 
-  xspeed: 4, 
+  x: 300, //position of the eye
+  y: 200, //position of the eye
+  xspeed: 4, //the speed of the eye
   yspeed: -3
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 400); //the background size
 
 }
 
 function draw() {
-  background("gray");
-  move();
+  background("gray"); // the background color
+  move(); //these functions make the eye bounce around
   bounce();
   display();
   
@@ -25,7 +25,7 @@ function draw() {
 
 function bounce() {
   
-  if (ball.x > width || ball.x < 0) {
+  if (ball.x > width || ball.x < 0) { //when the eye hit the edge of the canvas it bounce back the oppisite direction
     ball.xspeed = ball.xspeed * -1;
   } 
   
@@ -35,15 +35,15 @@ function bounce() {
 }
 
 function display() {
-  stroke(255);
+  stroke(255); //this is the setup of the eye
   strokeWeight(4);
   fill(mouseX, mouseY);
-  ellipse(ball.x, ball.y, 100, 50);
+  ellipse(ball.x, ball.y, 100, 50); 
   fill("red");
   ellipse(ball.x, ball.y, 50,50);
 }
 
-function move() {
+function move() { //this is the move function of the eye
   ball.x = ball.x + ball.xspeed;
   ball.y = ball.y + ball.yspeed;
   
